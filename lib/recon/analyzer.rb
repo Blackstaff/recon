52d930f7d512095a588961a206e6d9b0cab3c9fd
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2014-10-20 15:44:46 +0200

    Add Analyzer class skeleton

--- lib/recon/analyzer.rb
+++ lib/recon/analyzer.rb
@@ -1,6 +1,31 @@
+require 'ruby_parser'
+require 'sexp_processor'
 require_relative 'util/project_scanner'
 
 module Analyzer
-  class Analyzer
+  class Analyzer < SexpProcessor
+    def initialize
+      super()
+    end
+
+    def analyze
+
+    end
+
+    #########################################
+    # Process methods:
+
+    def process_class
+
+    end
+
+    def process_defn
+
+    end
+
+    def process_if
+
+    end
+
   end
 end
