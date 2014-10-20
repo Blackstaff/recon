0ada9c81a61870e3c1a64a37e4a70a99f3aabb8a
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2014-10-20 16:19:12 +0200

    Add spec for Analyzer

---
+++ spec/recon/analyzer/analyzer_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Analyzer do
+  describe '#analyze' do
+    before :all do
+      @result = Analyzer::Analyzer.new.analyze(File.dirname(__FILE__) + '/../../../lib')
+      @classes, @methods, @smells = @result
+    end
+
+    it 'returns an Array' do
+      expect( @result.respond_to?(:to_ary) ).to be true
+    end
+
+    it 'returns classes data' do
+      expect( @classes ).not_to be_empty
+      expect( @classes.map {|x| x.name } ).to include('Analyzer')
+    end
+
+    it 'returns methods data' do
+      expect( @methods ).not_to be_empty
+      expect( @methods.map {|x| x.name } ).to include('analyze')
+    end
+
+  end
+end
